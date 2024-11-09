784a9e8a23af945f60f2ecc9ee1234c1ee4f782b
Fishybearr <76235767+Fishybearr@users.noreply.github.com>
2024-11-09 15:17:25 -0500

    Add initiall html page
    Used express to send html client connection

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import Groq from "groq-sdk";
 import { key } from './key.js';
+import express from "express"
+
+const PORT = 3000;
+const app = express();
 
 const groq = new Groq({apiKey:key});
+
+//Initial connection
+app.get('/', (req, res) => {
+  res.sendFile(import.meta.dirname + '/client.html');
+});
+
+app.listen(PORT, () =>{});
 
 async function main() {
   const completion = await groq.chat.completions
@@ -16,7 +27,7 @@ async function main() {
     })
     .then((chatCompletion) => {
       console.log(chatCompletion.choices[0]?.message?.content || "");
-    });
+    }); 
 }
 
 main();
